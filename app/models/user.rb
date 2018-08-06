1377f6d1a53125c292d057a2255e2db60a85e6f2
maxime-manicourt <maxime.manicourt@gmail.com>
2018-08-06 19:29:47 +0200

    test email should be unique

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  attr_accessor :firs_name, :last_name, :email
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/new'
+  get 'user/create'
+  get 'user/edit'
+  get 'user/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
